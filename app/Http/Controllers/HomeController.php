453ab2507ef21a6cf29d970b84588797596ed1fb
Fajarammar <ammar@km.solutif.co.id>
2022-12-18 17:48:10 +0700

    Home Page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,12 +7,65 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
+use App\Models\Pelanggan;
+
+
+date_default_timezone_set("Asia/Jakarta");
 
 class HomeController extends Controller
 {
     public function index()
     {
-            return view('home');
+            $userid = Auth::user()->id;
+            $nama = User::where('id' , $userid)->value('name');
+            $today = date('d F Y');
+            $day = date('N');
+            if($day == '1')
+            {
+                $dayname = 'Senin';
+            }
+            elseif($day == '2')
+            {
+                $dayname = 'Selasa';
+            }
+            elseif($day == '3')
+            {
+                $dayname = 'Rabu';
+            }
+            elseif($day == '4')
+            {
+                $dayname = 'Kamis';
+            }
+            elseif($day == '5')
+            {
+                $dayname = 'Jumat';
+            }
+            elseif($day == '6')
+            {
+                $dayname = 'Sabtu';
+            }
+            else{
+                $dayname = 'Minggu';
+            }
+            $time = date('H:i:s');
+            
+            if($time >= '06:00:00' && $time < '18:00:00')
+            {
+                $iconday = "icon-sun";
+            }
+            else{
+                $iconday = "icon-moon";
+            }
+
+            $sumdata = Pelanggan::where('user_id' , $userid)->count();
+
+            return view('home' , compact(
+                'nama',
+                'today',
+                'iconday',
+                'sumdata',
+                'dayname'
+            ));
     }
 
     // public function search(Request $request)
